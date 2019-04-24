6b5dd5e660f4603da537dcb924d9b23f6e41bedd
Lars <lars1@users.noreply.github.com>
2019-04-24 11:32:44 +0200

    Copied demo code, a long list

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
 export class App {
-  message = 'Hello World!';
+  items = App.createItems();
+  
+  static createItems() {
+    let items = [];
+    for (let i=0; i<200; i++) {
+      items.push({ id: `${i}`, value: `Test ${i*i}`});
+    }
+    return items;
+  }
 }
